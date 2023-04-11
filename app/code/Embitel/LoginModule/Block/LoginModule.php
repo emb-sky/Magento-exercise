a1defefd819799a50213f86645ea13bdd2efdd79
Akash <emb-akasmah@embitel.com>
2023-04-11 19:03:44 +0530

    added Employee module

--- app/code/Embitel/LoginModule/Block/LoginModule.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Embitel\LoginModule\Block;
-class Newmodule extends \Magento\Framework\View\Element\Template
-{
-    public function _prepareLayout()
-    {
-        return parent::_prepareLayout();
-    }
-}
